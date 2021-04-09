2999c367690a9267c1a62cf03d65c340433dff15
Minh Vu <minhd_vu@yahoo.com>
2021-04-09 10:31:47 -0400

    changed to regression

--- baseline.py
+++ baseline.py
@@ -15,21 +15,29 @@ def main():
     train_df = pd.read_csv("data/train.csv")
     train_df = train_df[["comment_text", "target"]]
     train_df = clean_text(train_df, "comment_text")
-    train_df["target"] = class_labels(train_df["target"])
+    # train_df["target"] = class_labels(train_df["target"])
     train_df.columns = ["text", "labels"]
 
+    # Preparing eval data
     eval_df = pd.read_csv("data/test_public_expanded.csv")
     eval_df = eval_df[["comment_text", "toxicity"]]
     eval_df = clean_text(eval_df, "comment_text")
-    eval_df["toxicity"] = class_labels(eval_df["toxicity"])
+    # eval_df["toxicity"] = class_labels(eval_df["toxicity"])
     eval_df.columns = ["text", "labels"]
 
-    train_df.to_csv("data/train.tsv", sep=",", index=False)
-    eval_df.to_csv("data/eval.tsv", sep=",", index=False)
-    # Preparing eval data
+    train_df.to_csv("data/train_clean.csv", sep=",", index=False)
+    eval_df.to_csv("data/eval_clean.csv", sep=",", index=False)
 
     # Optional model configuration
-    model_args = ClassificationArgs(num_train_epochs=1, lazy_loading=True, lazy_labels_column=1, lazy_text_column=0, lazy_delimiter=',')
+    model_args = ClassificationArgs(
+        num_train_epochs=1,
+        lazy_loading=True,
+        lazy_labels_column=1,
+        lazy_text_column=0,
+        lazy_delimiter=',',
+        regression=True
+    )
+
     # Create a ClassificationModel
     model = ClassificationModel(
         "roberta",
@@ -39,15 +47,16 @@ def main():
     )
 
     # Train the model
-    #pdb.set_trace()
-    model.train_model("data/train.tsv")
+    # pdb.set_trace()
+    model.train_model("data/train_clean.csv")
 
     # Evaluate the model
-    #pdb.set_trace()
-    result, model_outputs, wrong_predictions = model.eval_model("data/eval.tsv")
+    # pdb.set_trace()
+    result, model_outputs, wrong_predictions = model.eval_model("data/eval_clean.csv")
 
     # Make predictions with the model
     # predictions, raw_outputs = model.predict(["you are trash"])
+
 
 if __name__ == "__main__":
     main()
