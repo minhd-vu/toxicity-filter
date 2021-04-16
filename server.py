06577634af7a04b93a909126858f4ac27a2d8595
Minh Vu <minhd_vu@yahoo.com>
2021-04-16 06:34:47 -0400

    update server

--- server.py
+++ server.py
@@ -2,19 +2,25 @@
 #!/usr/local/bin/python3
 
 from simpletransformers.classification import ClassificationModel
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+model = ClassificationModel(
+    "roberta", "outputs/checkpoint-183643-epoch-1",
+    use_cuda=False
+)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    global model
+
+    if request.method == "POST":
+        predictions, raw_outputs = model.predict([request.json["message"]])
+        return str(predictions)
+    elif request.method == "GET":
+        return "<h1>Toxcity Filter API</h1>"
 
 
-def main():
-    model = ClassificationModel(
-        "roberta", "outputs/checkpoint-183643-epoch-1",
-        use_cuda=False
-    )
-
-    while True:
-        text = input("> ")
-        predictions, raw_outputs = model.predict([text])
-        print(predictions, raw_outputs)
-
-
-if __name__ == "__main__":
-    main()
+app.run()
